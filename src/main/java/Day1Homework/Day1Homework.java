cb6f08d2041be0ccf8e3eed07d730281a39d970f
Ray <54691205+rjenaa@users.noreply.github.com>
2020-09-01 16:32:27 -0400

    This is day 1 homework

---
+++ src/main/java/Day1Homework/Day1Homework.java
@@ -0,0 +1,12 @@
+package Day1Homework;
+import java.util.Scanner;
+public class Day1Homework {
+    public static void main(String[] args) {
+        Scanner myColor = new Scanner(System.in);
+        System.out.println("Please enter your favorite color:");
+        String favColor = myColor.nextLine();
+        System.out.println("Your favorite color is: " + favColor);
+
+
+    }
+}
